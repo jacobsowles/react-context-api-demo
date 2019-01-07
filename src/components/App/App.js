10164902559e82491ff20bb5f3db7b1b6560258d
Jacob Sowles <jrsowles@gmail.com>
2019-01-07 12:21:12 -0800

    refactor: change App from PureComponent to function component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,14 @@
-import React, { PureComponent } from "react";
+import React from "react";
 
 import ThemeIndicator from "../ThemeIndicator";
 import ThemeToggle from "../ThemeToggle";
 import "./App.css";
 
-class App extends PureComponent {
-  render() {
-    return (
-      <div className="app">
-        <ThemeIndicator />
-        <ThemeToggle />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="app">
+    <ThemeIndicator />
+    <ThemeToggle />
+  </div>
+);
 
 export default App;
