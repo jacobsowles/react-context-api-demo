00424158c520bcd8294b09d2c3a0dda08b2322ca
Jacob Sowles <jrsowles@gmail.com>
2019-01-07 15:33:53 -0800

    refactor: clean up syntax

--- src/components/ThemeIndicator/ThemeIndicator.js
+++ src/components/ThemeIndicator/ThemeIndicator.js
@@ -2,15 +2,13 @@ import React from "react";
 
 import "./ThemeIndicator.css";
 
-const ThemeIndicator = ({ theme }) => {
-  return (
-    <div className="theme-indicator">
-      <h1>
-        {theme} Moose, age {theme === "smol" ? "3" : "6"} months
-      </h1>
-      <img src={`./${theme}-puppy.png`} alt={`${theme} puppy`} />
-    </div>
-  );
-};
+const ThemeIndicator = ({ theme }) => (
+  <div className="theme-indicator">
+    <h1>
+      {theme} Moose, age {theme === "smol" ? "3" : "6"} months
+    </h1>
+    <img src={`./${theme}-puppy.png`} alt={`${theme} puppy`} />
+  </div>
+);
 
 export default ThemeIndicator;
--- src/components/ThemeToggle/ThemeToggle.js
+++ src/components/ThemeToggle/ThemeToggle.js
@@ -2,12 +2,10 @@ import React from "react";
 
 import "./ThemeToggle.css";
 
-const ThemeToggle = ({ theme, toggleTheme }) => {
-  return (
-    <button className="theme-toggle" onClick={toggleTheme}>
-      Watch me grow{theme === "smol" ? " up" : "...down?"}
-    </button>
-  );
-};
+const ThemeToggle = ({ theme, toggleTheme }) => (
+  <button className="theme-toggle" onClick={toggleTheme}>
+    Watch me grow{theme === "smol" ? " up" : "...down?"}
+  </button>
+);
 
 export default ThemeToggle;
