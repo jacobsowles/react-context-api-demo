bf6987c2ed039d1e430e3b0748ccc657f9134e4f
Jacob Sowles <jrsowles@gmail.com>
2019-01-07 15:37:47 -0800

    refactor: clean up syntax

--- src/components/ThemeIndicator/ThemeIndicator.js
+++ src/components/ThemeIndicator/ThemeIndicator.js
@@ -3,23 +3,20 @@ import React from "react";
 import { ThemeConsumer } from "../ThemeContext";
 import "./ThemeIndicator.css";
 
-const ThemeIndicator = () => {
-  return (
-    <ThemeConsumer>
-      {context => (
-        <div className="theme-indicator">
-          <h1>
-            {context.name} Moose, age {context.name === "smol" ? "3" : "6"}{" "}
-            months
-          </h1>
-          <img
-            src={`./${context.name}-puppy.png`}
-            alt={`${context.name} puppy`}
-          />
-        </div>
-      )}
-    </ThemeConsumer>
-  );
-};
+const ThemeIndicator = () => (
+  <ThemeConsumer>
+    {context => (
+      <div className="theme-indicator">
+        <h1>
+          {context.name} Moose, age {context.name === "smol" ? "3" : "6"} months
+        </h1>
+        <img
+          src={`./${context.name}-puppy.png`}
+          alt={`${context.name} puppy`}
+        />
+      </div>
+    )}
+  </ThemeConsumer>
+);
 
 export default ThemeIndicator;
--- src/components/ThemeToggle/ThemeToggle.js
+++ src/components/ThemeToggle/ThemeToggle.js
@@ -3,16 +3,14 @@ import React from "react";
 import { ThemeConsumer } from "../ThemeContext";
 import "./ThemeToggle.css";
 
-const ThemeToggle = () => {
-  return (
-    <ThemeConsumer>
-      {context => (
-        <button className="theme-toggle" onClick={context.toggleTheme}>
-          Watch me grow{context.name === "smol" ? " up" : "...down?"}
-        </button>
-      )}
-    </ThemeConsumer>
-  );
-};
+const ThemeToggle = () => (
+  <ThemeConsumer>
+    {context => (
+      <button className="theme-toggle" onClick={context.toggleTheme}>
+        Watch me grow{context.name === "smol" ? " up" : "...down?"}
+      </button>
+    )}
+  </ThemeConsumer>
+);
 
 export default ThemeToggle;
